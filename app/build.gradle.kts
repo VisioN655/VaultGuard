5ac6a87614fe91d4198f2f6a73a142a9e13e663a
VisioN655 <affanatik5565@gmail.com>
2026-01-05 03:03:47 +0100

    FragmentContainerView in das Projekt hinzugefügt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.cardview)
     implementation(libs.firebase.auth)
     implementation(libs.recyclerview)
+    implementation(libs.fragment)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
