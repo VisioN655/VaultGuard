8857cc84d174e3813dcdb1d388768acf31de2747
VisioN655 <affanatik5565@gmail.com>
2026-01-26 04:26:38 +0100

    Erste Logik für das Laden der Karten in die Passwortliste implementiert

--- app/src/main/java/com/example/vaultguard/PasswordListFragment.java
+++ app/src/main/java/com/example/vaultguard/PasswordListFragment.java
@@ -1,64 +1,64 @@
 package com.example.vaultguard;
 
 import android.os.Bundle;
-
 import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import com.google.android.material.card.MaterialCardView;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.*;
+import com.google.firebase.firestore.*;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PasswordListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class PasswordListFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    LinearLayout passwordContainer;
+    LayoutInflater cardInflater;
+    MaterialCardView passwordCard;
+    TextView emptyText;
+    TextView card_title;
+    TextInputEditText card_email;
+    TextInputEditText card_password;
+    ImageView card_image;
+    View view;
+    View card;
 
     public PasswordListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PasswordListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PasswordListFragment newInstance(String param1, String param2) {
-        PasswordListFragment fragment = new PasswordListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_password_list, container, false);
+        passwordContainer = view.findViewById(R.id.password_container);
+        emptyText = view.findViewById(R.id.empty_list);
+        cardInflater = LayoutInflater.from(getContext());
+
+        for (int i = 0; i < 5; i++) {
+            card = cardInflater.inflate(R.layout.item_password, passwordContainer, false);
+            passwordCard = card.findViewById(R.id.password_card);
+            card_title = card.findViewById(R.id.platform_title);
+            card_email = card.findViewById(R.id.item_email_input);
+            card_password = card.findViewById(R.id.item_password_input);
+            card_image = card.findViewById(R.id.platform_icon);
+
+            passwordCard.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getContext(), "Card klick!", Toast.LENGTH_SHORT).show();
+                }
+            });
+            passwordContainer.addView(card);
         }
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_password_list, container, false);
+        if (passwordContainer.getChildCount() > 0) {
+            emptyText.setVisibility(View.GONE);
+        } else {
+            emptyText.setVisibility(View.VISIBLE);
+        }
+        return view;
     }
 }
