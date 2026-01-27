7cfe60484acefd6dc7b41f4fd193e886a7f751f1
Affan Atik <affanatik5565@gmail.com>
2026-01-28 02:01:45 +0300

    Logik für die Weiterleitung des Drückens vom Hinzufügen-Button an den Passwort-Erstellen-Screen implementiert

--- app/src/main/java/com/example/vaultguard/HomeScreenActivity.java
+++ app/src/main/java/com/example/vaultguard/HomeScreenActivity.java
@@ -1,14 +1,21 @@
 package com.example.vaultguard;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.card.MaterialCardView;
+
 public class HomeScreenActivity extends AppCompatActivity {
+    MaterialCardView addButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        addButton = findViewById(R.id.add_button);
 
         if (savedInstanceState == null) {
             getSupportFragmentManager()
@@ -16,5 +23,14 @@ public class HomeScreenActivity extends AppCompatActivity {
                     .replace(R.id.fragment_container, new PasswordListFragment())
                     .commit();
         }
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            Intent create_password = new Intent(HomeScreenActivity.this, AddPasswordActivity.class);
+            @Override
+            public void onClick(View v) {
+                startActivity(create_password);
+            }
+        });
+
     }
 }
