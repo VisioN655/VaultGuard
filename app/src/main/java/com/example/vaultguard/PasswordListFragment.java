93151f2d537c795fdb31ad646848c9f89e0ae832
Affan Atik <affanatik5565@gmail.com>
2026-01-28 02:03:15 +0300

    Passwortliste an Firestore angebunden und nutzerspezifische Daten geladen

--- app/src/main/java/com/example/vaultguard/PasswordListFragment.java
+++ app/src/main/java/com/example/vaultguard/PasswordListFragment.java
@@ -1,18 +1,27 @@
 package com.example.vaultguard;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.card.MaterialCardView;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.firebase.*;
+import com.google.firebase.auth.*;
 import com.google.firebase.firestore.*;
+import com.bumptech.glide.Glide;
 
 
 public class PasswordListFragment extends Fragment {
@@ -27,38 +36,75 @@ public class PasswordListFragment extends Fragment {
     ImageView card_image;
     View view;
     View card;
+    FirebaseFirestore db;
+    FirebaseAuth auth;
+    FirebaseUser user;
+    String uid;
 
     public PasswordListFragment() {
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
+        user = auth.getCurrentUser();
+        uid = user.getUid();
         view = inflater.inflate(R.layout.fragment_password_list, container, false);
         passwordContainer = view.findViewById(R.id.password_container);
         emptyText = view.findViewById(R.id.empty_list);
         cardInflater = LayoutInflater.from(getContext());
 
-        for (int i = 0; i < 5; i++) {
-            card = cardInflater.inflate(R.layout.item_password, passwordContainer, false);
-            passwordCard = card.findViewById(R.id.password_card);
-            card_title = card.findViewById(R.id.platform_title);
-            card_email = card.findViewById(R.id.item_email_input);
-            card_password = card.findViewById(R.id.item_password_input);
-            card_image = card.findViewById(R.id.platform_icon);
+        db.collection("users")
+                .document(uid)
+                .collection("passwords")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
 
-            passwordCard.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Toast.makeText(getContext(), "Card klick!", Toast.LENGTH_SHORT).show();
-                }
-            });
-            passwordContainer.addView(card);
-        }
-        if (passwordContainer.getChildCount() > 0) {
-            emptyText.setVisibility(View.GONE);
-        } else {
-            emptyText.setVisibility(View.VISIBLE);
-        }
+                        if (!task.isSuccessful()) {
+                            emptyText.setVisibility(View.VISIBLE);
+                            return;
+                        }
+
+                        if(task.isSuccessful()) {
+                            QuerySnapshot query = task.getResult();
+
+                            for (int i = 0; i < query.getDocuments().size(); i++) {
+                                DocumentSnapshot doc = query.getDocuments().get(i);
+                                card = cardInflater.inflate(R.layout.item_password, passwordContainer, false);
+                                passwordCard = card.findViewById(R.id.password_card);
+                                card_title = card.findViewById(R.id.platform_title);
+                                card_email = card.findViewById(R.id.item_email_input);
+                                card_password = card.findViewById(R.id.item_password_input);
+                                card_image = card.findViewById(R.id.platform_icon);
+
+
+                                String title = doc.getString("title");
+                                String email = doc.getString("email");
+                                String password = doc.getString("password");
+                                String imageURL = doc.getString("imageURL");
+
+                                card_title.setText(title);
+                                card_email.setText(email);
+                                card_password.setText(password);
+                                Glide.with(PasswordListFragment.this)
+                                        .load(imageURL)
+                                        .into(card_image);
+
+                                passwordCard.setOnClickListener(new View.OnClickListener() {
+                                    @Override
+                                    public void onClick(View v) {
+                                        Toast.makeText(getContext(), "Card klick!", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                                passwordContainer.addView(card);
+                            }
+                        }
+                    }
+                });
+
         return view;
     }
 }
